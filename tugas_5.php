f89e9a219d848a2ad97eaaf7b0cfb9157b77851d
snowiezoe <izzaayumi@gmail.com>
2026-06-09 21:34:52 +0700

    form html

---
+++ tugas_5.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 5</title>
+</head>
+<body>
+    <form action="">
+        <div>
+            <label for="">Name</label>
+            <input name="Nama" placeholder="Masukkan nama" id="">
+        </div>
+        <div>
+            <label for="">Alamat</label>
+             <input name="Alamat" placeholder="Masukkan alamat" id="">
+        </div>
+        <div>
+            <button>Submit</button>
+        </div>
+    </form>
+</body>
+</html>
