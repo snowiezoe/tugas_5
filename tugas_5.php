45ff40c48e932a6a32a5629fe3e21e0526cd081d
snowiezoe <izzaayumi@gmail.com>
2026-06-09 22:45:23 +0700

    form-finish

--- tugas_5.php
+++ tugas_5.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas 5</title>
+    <title>Membuat Form Sederhana</title>
+    <link rel="stylesheet" href="filecss.css">
     <style>
         form{
-            width: 100px;
+            width: 500px;
+            height: 130px;
             margin: 10px;
             font-family:'Times New Roman';
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
@@ -20,33 +22,56 @@
             margin-bottom: 5px;
             font-weight: bold;
         }
+        div {
+            display: flex;
+            justify-content: center;
+            padding: 10px;
+            margin-right: 10px;
+        }
+        label{
+            margin-right: 5px;
+        }
+        button{
+            background-color: white;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            border-radius: 30px;
+            font-family: 'Times New Roman';
+        }
     </style>
 </head>
-<body>
-    <form action="">
-        <div>
-            <label for="">Name</label>
-            <input name="Nama" placeholder="Masukkan nama" id="">
+<body class="body-html">
+    <form>
+        <div class="nama-label">
+            <label>Nama</label> <br>
+            <input name="nama" type="text" placeholder="Masukkan nama">
         </div>
-        <div>
-            <label for="">Alamat</label>
-             <input name="Alamat" placeholder="Masukkan alamat" id="">
+
+        <div class="alamat-label">
+            <label>Alamat</label> <br>
+            <input name="alamat" type="text" placeholder="Masukkan alamat">
         </div>
+
         <div>
             <button>Submit</button>
         </div>
     </form>
-        <?php
-    $nama = @$_GET['nama'];
-    $alamat = @$_GET['alamat'];
 
-    if($nama){
-        echo "<strong>Nama:</strong> {$nama} <br>"
-    }
+<?php # membuka tag PHP
 
-    if($alamat){
-        echo "<strong>Alamaat:</strong> {$alamat} <br>"
-    }
-    ?>
+$nama = @$_GET['nama'];
+$alamat = @$_GET['alamat'];
+
+# di sini nanti kita akan tampilkan variabel $nama dan $alamat
+if ($nama) {
+    echo "<strong>Nama:</strong> {$nama} <br>";
+}
+
+if ($alamat) {
+    echo "<strong>Alamat:</strong> {$alamat} <br>";
+}
+
+# jangan lupa tutup tag PHP
+?>
+
 </body>
 </html>
